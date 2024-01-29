06d27b47c733d70442a10885eb2fd7406057dfc3
Luke Barousse <luke.barousse@gmail.com>
2024-01-28 20:54:31 -0600

    fix table load issues

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,7 +1,19 @@
+COPY company_dim
+FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/company_dim.csv'
+DELIMITER ',' CSV HEADER;
+
 COPY job_postings_fact
 FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/job_postings_fact.csv'
 DELIMITER ',' CSV HEADER;
 
+COPY skills_dim
+FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/skills_dim.csv'
+DELIMITER ',' CSV HEADER;
+
+COPY skills_job_dim
+FROM '/Users/lukebarousse/Developer/DataNerd/Data_Jobs_SQL_Tutorial/csv_files/skills_job_dim.csv'
+DELIMITER ',' CSV HEADER;
+
 -- SELECT *
--- FROM job_postings_fact
+-- FROM skills_job_dim
 -- LIMIT 5;
